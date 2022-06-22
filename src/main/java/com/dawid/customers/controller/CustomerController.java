15a6f3c85e24e89630828a5a0bdaabc54f529800
Dawid Krogulec <Dawid.Krogulec@acoustic.co>
2022-06-22 15:49:40 +0200

    Implement getAll

--- src/main/java/com/dawid/customers/controller/CustomerController.java
+++ src/main/java/com/dawid/customers/controller/CustomerController.java
@@ -1,4 +1,33 @@
 package com.dawid.customers.controller;
 
+import com.dawid.customers.model.Customer;
+import com.dawid.customers.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/customer")
 public class CustomerController {
+@Autowired
+    private CustomerRepository customerRepository;
+    @GetMapping("/all")
+    public List<Customer> findAllUsers() {
+
+        return customerRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> findUserById(@PathVariable(value = "id") long id) {
+
+        return null;
+    }
+    @PostMapping
+    public Customer saveUser(@Validated @RequestBody Customer customer) {
+
+        return customer;
+    }
 }
--- src/main/java/com/dawid/customers/repository/CustomerRepository.java
+++ src/main/java/com/dawid/customers/repository/CustomerRepository.java
@@ -2,12 +2,14 @@ package com.dawid.customers.repository;
 
 import com.dawid.customers.model.Customer;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
 
-        List<Customer> findByLastName(String lastName);
+        List<Customer> findAll();
 
         Customer findById(long id);
 }
