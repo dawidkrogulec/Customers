661d0827a68b640ec3c48ee7b79a0dc5ca97ac45
Dawid Krogulec <Dawid.Krogulec@acoustic.co>
2022-06-29 10:10:14 +0200

    Added 2 endpoints (Delete and Put)

--- src/main/java/com/dawid/customers/controller/CustomerController.java
+++ src/main/java/com/dawid/customers/controller/CustomerController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.Id;
 import java.util.List;
 
 @RestController
