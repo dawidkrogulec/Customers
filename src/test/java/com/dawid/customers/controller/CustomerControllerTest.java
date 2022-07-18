d029878d1504f8a408ad88bef9bbb826457fc6c6
Dawid Krogulec <Dawid.Krogulec@acoustic.co>
2022-07-18 12:45:12 +0200

    unit test when & verify added

--- src/test/java/com/dawid/customers/controller/CustomerControllerTest.java
+++ src/test/java/com/dawid/customers/controller/CustomerControllerTest.java
@@ -4,11 +4,15 @@ import com.dawid.customers.model.Customer;
 import com.dawid.customers.repository.CustomerRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
+import static org.mockito.ArgumentMatchers.any;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CustomerControllerTest {
@@ -16,12 +20,16 @@ class CustomerControllerTest {
     @Mock
     CustomerRepository repository;
 
+    @InjectMocks
+    CustomerController controller;
+
     @Test
     void updateCustomerNotFound() {
         //given
-        
         //when
-        //than
+        when(repository.findById(any())).thenReturn(Fixtures.CUSTOMER_NOT_FOUND);
+        //then
+        verify(controller.updateCustomer(any(), any()).equals(Fixtures.CUSTOMER_NOT_FOUND_MSG));
     }
 
     @Test
@@ -30,6 +38,8 @@ class CustomerControllerTest {
 
     private static class Fixtures{
         private final static Optional<Customer> CUSTOMER_FOUND = Optional.of(new Customer(3L, "Dawid", "Krogulec", 39));
+        private final static Customer CUSTOMER_WITH_NO_ID = new Customer("Dawid", "Krogulec", 39);
         private final static Optional<Customer> CUSTOMER_NOT_FOUND = Optional.empty();
+        private final static String CUSTOMER_NOT_FOUND_MSG = "Customer not found";
     }
 }
