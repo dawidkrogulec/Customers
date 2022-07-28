323b842a5537b12e4a7364b8a6803c067e687842
Dawid Krogulec <Dawid.Krogulec@acoustic.co>
2022-07-28 13:11:56 +0200

    New tests added

--- src/test/java/com/dawid/customers/controller/CustomerControllerTest.java
+++ src/test/java/com/dawid/customers/controller/CustomerControllerTest.java
@@ -32,33 +32,11 @@ import static org.mockito.Mockito.*;
             //given
             //when
             when(repository.findAll()).thenReturn((Fixtures.CUSTOMER_LIST));
-            assertEquals(controller.findAllCustomers(), Fixtures.CUSTOMER_NOT_FOUND_MSG);
+            List<Customer> result = controller.findAllCustomers();
+            assertEquals(3,result.size());
 
 
             //then
-        }
-
-        @Test
-        void shouldNotUpdateCustomer() {
-            //given
-            when(repository.findById(any())).thenReturn(Fixtures.CUSTOMER_NOT_FOUND);
-            //when
-            assertEquals(controller.updateCustomer(any(), Fixtures.CUSTOMER_WITH_NO_ID),Fixtures.CUSTOMER_UPDATED_MSG);
-            //then
-            verify(repository, times(1)).findById(any());
-            verify(repository, times(0)).save(any());
-        }
-
-        @Test
-        void shouldUpdateCustomer() {
-            //given
-            when(repository.findById(any())).thenReturn(Optional.of(Fixtures.CUSTOMER_FOUND));
-            //when
-            assertEquals(controller.updateCustomer(any(), Fixtures.CUSTOMER_WITH_NO_ID), Fixtures.CUSTOMER_UPDATED_MSG);
-            //then
-            verify(repository, times(1)).findById(any());
-            verify(repository, times(1)).save(any());
-
         }
         @Test
         void shouldReturnZeroCustomers() {
@@ -68,9 +46,32 @@ import static org.mockito.Mockito.*;
             List<Customer> result = controller.findAllCustomers();
             assertEquals(0, result.size());
         }
+        @Test
+        void shouldNotUpdateCustomer() {
+            //given
+            when(repository.findById(any())).thenReturn(Fixtures.CUSTOMER_NOT_FOUND);
+            //when
+            assertEquals(Fixtures.CUSTOMER_NOT_FOUND_MSG, controller.updateCustomer(1L, Fixtures.CUSTOMER_WITH_NO_ID));
+            //then
+            verify(repository, times(1)).findById(any());
+            verify(repository, times(0)).save(any());
+        }
+
+        @Test
+        void shouldUpdateCustomer() {
+            //given
+            when(repository.findById(any())).thenReturn(Fixtures.CUSTOMER_FOUND);
+            //when
+            assertEquals(Fixtures.CUSTOMER_UPDATED_MSG, controller.updateCustomer(any(), Fixtures.CUSTOMER_WITH_NO_ID));
+            //then
+            verify(repository, times(1)).findById(any());
+            verify(repository, times(1)).save(any());
+
+        }
+
     private static class Fixtures{
 
-        private final static Customer CUSTOMER_FOUND = new Customer(3L, "Dawid", "Krogulec", 39);
+        private final static Optional<Customer> CUSTOMER_FOUND = Optional.of(new Customer(3L, "Dawid", "Krogulec", 39));
         private final static Customer CUSTOMER_WITH_NO_ID = new Customer("Dawid", "Krogulec", 39);
         private final static List<Customer> NO_CUSTOMERS_FOUND = new ArrayList<>();
         //listę customerów
@@ -79,9 +80,8 @@ import static org.mockito.Mockito.*;
                 new Customer(2L, "Dawid", "Krogulec", 40));
 
         private static final Optional<Customer> CUSTOMER_NOT_FOUND = Optional.empty();
-
-
-        private final static String CUSTOMER_NOT_FOUND_MSG = "Customer not found";
-        private final static String CUSTOMER_UPDATED_MSG = "Customer updated succesfully";
+        private final static Customer CUSTOMER_WITH_ID = new Customer(1L, "Lukasz", "Krogulec", 39);
+        private final static String CUSTOMER_NOT_FOUND_MSG = "Customer with id: 1 not found";
+        private final static String CUSTOMER_UPDATED_MSG = "Customer updated successfully";
     }
 }
