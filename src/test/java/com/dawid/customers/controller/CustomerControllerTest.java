545fdcf756239e169c16286fd8b38e95c67fe5b6
Dawid Krogulec <Dawid.Krogulec@acoustic.co>
2022-08-01 10:58:42 +0200

    All tests done

--- src/test/java/com/dawid/customers/controller/CustomerControllerTest.java
+++ src/test/java/com/dawid/customers/controller/CustomerControllerTest.java
@@ -17,7 +17,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-
 @ExtendWith(MockitoExtension.class)
 class CustomerControllerTest {
 
